17e1fe6bf8cff7067e2fab6aeec294e0a9672ceb
Valeria Petrovich <valeria.petrovich@northitgroup.eu>
2022-10-09 12:41:21 +0300

    feat: corrections

--- learn-projects/react-wiki-2/src/components/Card/CardDetails.js
+++ learn-projects/react-wiki-2/src/components/Card/CardDetails.js
@@ -2,12 +2,11 @@ import React from "react";
 import useMeals from "../../hoock/useMeals";
 import { useParams } from "react-router-dom";
 import styles from '../../styles/CardDetails.module.scss';
-import useIngredients from "../../hoock/useIngredients";
+import TableIngredients from "../Table";
 
 const CardDetails = () => {
   let { id } = useParams();
   const { fetcheMeals } = useMeals();
-  const {} = useIngredients();
   let display;
   display = fetcheMeals.map((x) => x.map((el) => {
     if (el.idMeal === id) {
@@ -34,7 +33,8 @@ const CardDetails = () => {
               </div>
               <div className="">
                 <span className={styles['curent-text']}>Ingredients: </span>
-                <span className={styles['tex-card']}> {el.strIngredient1}</span>
+                <span className={styles['tex-card']}> {Object.values(el).slice(9,18)}</span>
+                <TableIngredients/>
               </div>
               <div className="">
                 <span className={styles['curent-text']}>
@@ -51,44 +51,6 @@ const CardDetails = () => {
   })
   );
   return <>{display}</>;
-
-  /*
-    return (
-      <div className="container d-flex justify-content-center">
-        <div className="d-flex flex-column gap-3">
-          <h1 className="">{name}</h1>
-          <img src={image} alt="" className="img-fluid" />
-  
-          {(() => {
-            if (status === "Dead") {
-              return <div className="badge bg-danger fs-5">{status} </div>;
-            } else if (status === "Alive") {
-              return <div className="badge bg-success fs-5">{status} </div>;
-            } else if (status === "unknown") {
-              return <div className="badge bg-secondary fs-5">{status}</div>;
-            }
-          })()}
-          <div className="content">
-            <div className="">
-              <span className="">Gender:</span>
-              {gender}
-            </div>
-            <div className="">
-              <span className="">Location:</span>
-              {location?.name}
-            </div>
-            <div className="">
-              <span className="">Origin:</span>
-              {origin?.name}
-            </div>
-            <div className="">
-              <span className="">Species:</span>
-              {species}
-            </div>
-          </div>
-        </div>
-      </div>
-    );*/
 };
 
 export default CardDetails;
